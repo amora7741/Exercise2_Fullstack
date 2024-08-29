7bfb9c71f04741aa9e2c70c7655c96b64610d8bc
amora7741 <mora7741@gmail.com>
2024-08-29 14:11:41 -0700

    Add rerouting middleware

---
+++ src/middleware.ts
@@ -0,0 +1,46 @@
+import { getToken } from 'next-auth/jwt';
+import { withAuth } from 'next-auth/middleware';
+import { NextResponse, NextRequest } from 'next/server';
+
+export default withAuth(
+  async function middleware(req: NextRequest) {
+    const pathName = req.nextUrl.pathname;
+
+    const isAuth = await getToken({ req });
+    const isLoginPath =
+      pathName.startsWith('/register') || pathName.startsWith('/login');
+
+    const sensitiveRoutes = ['/home'];
+
+    const isAccessingSensitiveRoute = sensitiveRoutes.some((route) =>
+      pathName.startsWith(route)
+    );
+
+    if (isLoginPath) {
+      if (isAuth) {
+        return NextResponse.redirect(new URL('/home', req.url));
+      }
+
+      return NextResponse.next();
+    }
+
+    if (!isAuth && isAccessingSensitiveRoute) {
+      return NextResponse.redirect(new URL('/login', req.url));
+    }
+
+    if (pathName === '/') {
+      return NextResponse.redirect(new URL('/home', req.url));
+    }
+  },
+  {
+    callbacks: {
+      async authorized() {
+        return true;
+      },
+    },
+  }
+);
+
+export const config = {
+  matcher: ['/', '/login', '/register', '/home/:path*'],
+};
