ce8ac149353fd1a07bd944dc912a467e1e170f5f
amora7741 <mora7741@gmail.com>
2024-08-30 17:32:10 -0700

    Show loading logo while fetching tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -2,28 +2,46 @@
 
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { LoaderCircle } from 'lucide-react';
 
 const TaskList = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getUserTasks = async () => {
-      const response = await axios.get('/api/tasks');
+      try {
+        const response = await axios.get('/api/tasks');
 
-      const parsedTasks: Task[] = response.data.tasks;
+        const parsedTasks: Task[] = response.data.tasks;
 
-      setTasks(parsedTasks);
+        setTasks(parsedTasks);
+      } catch (error) {
+      } finally {
+        setLoading(false);
+      }
     };
 
     getUserTasks();
   }, []);
 
   return (
-    <ul>
-      {tasks.map((task) => (
-        <li key={task.id}>{task.title}</li>
-      ))}
-    </ul>
+    <>
+      {loading ? (
+        <LoaderCircle className='size-10 text-blue-400 animate-spin mx-auto' />
+      ) : (
+        <ul className='space-y-4 max-h-[80vh] overflow-auto'>
+          {tasks.map((task) => (
+            <li className='p-4 bg-blue-300 rounded-lg' key={task.id}>
+              <div className='flex justify-between'>
+                <p>{task.title}</p>
+                <p>{task.priority} Priority</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 };
 
