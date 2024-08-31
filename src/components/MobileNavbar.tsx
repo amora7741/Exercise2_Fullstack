f8424bc44673ef6408e7154610c33c12638b6099
amora7741 <mora7741@gmail.com>
2024-08-30 20:01:05 -0700

    Fix navbar close button not showing

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -24,20 +24,21 @@ const MobileNavbar = ({ username }: { username: string }) => {
           <h1 className='text-2xl font-bold text-white'>TaskMaster</h1>
         </Link>
         <button onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
-          {isMobileMenuOpen ? (
-            <X size={28} className='text-white' />
-          ) : (
-            <Menu size={28} className='text-white' />
-          )}
+          <Menu size={28} className='text-white' />
         </button>
       </div>
 
       {isMobileMenuOpen && (
         <div className='fixed inset-0 z-50 bg-blue-400 p-6 flex flex-col md:hidden'>
-          <Link href='/home' className='flex items-center gap-2 mb-8'>
-            <BookCheck size={40} fill='white' />
-            <h1 className='text-3xl font-bold text-white'>TaskMaster</h1>
-          </Link>
+          <div className='flex justify-between items-center mb-8'>
+            <Link href='/home' className='flex items-center gap-2'>
+              <BookCheck size={40} fill='white' />
+              <h1 className='text-3xl font-bold text-white'>TaskMaster</h1>
+            </Link>
+            <button onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
+              <X size={28} className='text-white' />
+            </button>
+          </div>
           <SidebarOptions />
           <div className='mt-auto'>
             <LogoutPopover username={username} />
